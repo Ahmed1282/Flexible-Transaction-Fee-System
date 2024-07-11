dd794f1eb81b9877631927a7e8379c1b6a143c5d
Sameer <sameerkhan.as100@gmail.com>
2024-07-11 16:02:30 +0500

    Added db server

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import express from 'express';
+import sequelize from './config/db'
 import dotenv from 'dotenv';
 
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('DB Connection has been established successfully.');
+
+    // // Synchronize models
+    // await sequelize.sync({ force: true }); // Use force: true to drop and recreate tables
+    // console.log('All models were synchronized successfully.');
+
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
