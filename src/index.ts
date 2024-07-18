86f2d3501b1a6d612bc0a1d51bbe722a35fe22a4
Sameer <sameerkhan.as100@gmail.com>
2024-07-18 12:02:05 +0500

    Converting to typeORM

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import sequelize from './config/db';
+import AppDataSource from './config/db';
 import Country from './models/country'; // Ensure the path is correct
 import dotenv from 'dotenv';
-import Jurisdiction from './models/jurisdiction';
+// import Jurisdiction from './models/jurisdiction';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -19,6 +20,18 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization:', err);
+  });
+
+
 // app.get('/api', async (req, res) => {
 //   const result = await Jurisdictions.create({UserFeeShare: 100 , tax: 2,
 //     discount: 123,
@@ -31,33 +44,33 @@ app.use(express.json());
 
 // });
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('DB Connection has been established successfully.');
+// (async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log('DB Connection has been established successfully.');
 
-    // Explicitly synchronize the Country model
-    await Country.sync({ force: true, logging: console.log });
-    console.log('Country model synchronized successfully.');
+//     // Explicitly synchronize the Country model
+//     await Country.sync({ force: true, logging: console.log });
+//     console.log('Country model synchronized successfully.');
 
-    // Sync the models
-    console.log('Synchronizing models...');
-    await sequelize.sync({ force: true });
-    console.log('All models were synchronized successfully.');
+//     // Sync the models
+//     console.log('Synchronizing models...');
+//     await sequelize.sync({ force: true });
+//     console.log('All models were synchronized successfully.');
 
-    // Verify if the table exists
-    // const [results, metadata] = await sequelize.query("SELECT table_name FROM information_schema.tables WHERE table_schema='public';");
-    // console.log('Query Results:', results);
-    // console.log('Tables in the database:', results.map((row: any) => row.table_name));
+//     // Verify if the table exists
+//     // const [results, metadata] = await sequelize.query("SELECT table_name FROM information_schema.tables WHERE table_schema='public';");
+//     // console.log('Query Results:', results);
+//     // console.log('Tables in the database:', results.map((row: any) => row.table_name));
 
-    await Jurisdiction.sync({ force : true, logging: console.log});
-    console.log('Jurisdiction table was synchronized successfully.');
-    // Start the server
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-})();
+//     await Jurisdiction.sync({ force : true, logging: console.log});
+//     console.log('Jurisdiction table was synchronized successfully.');
+//     // Start the server
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// })();
 
