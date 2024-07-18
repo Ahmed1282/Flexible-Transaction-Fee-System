bdd7ef02321a7d392d08fdd7c4489ccffb72ea91
Ahmed Baig <ahmedbaig137@gmail.com>
2024-07-18 13:37:22 +0500

    Added country service, controller, validator and routes

---
+++ src/services/countryService.ts
@@ -0,0 +1,40 @@
+import { Repository } from 'typeorm';
+import AppDataSource from '../config/db';
+import Country from '../models/country';
+
+class CountryService {
+  private countryRepository: Repository<Country>;
+
+  constructor() {
+    this.countryRepository = AppDataSource.getRepository(Country);
+  }
+
+  async createCountry(data: Partial<Country>): Promise<Country> {
+    const country = this.countryRepository.create(data);
+    return this.countryRepository.save(country);
+  }
+
+  async getAllCountries(): Promise<Country[]> {
+    return this.countryRepository.find();
+  }
+
+  async getCountryById(id: number): Promise<Country | null> {
+    return this.countryRepository.findOneBy({ country_id: id });
+  }
+
+  async updateCountry(id: number, data: Partial<Country>): Promise<Country | null> {
+    const country = await this.countryRepository.findOneBy({ country_id: id });
+    if (!country) {
+      return null;
+    }
+    this.countryRepository.merge(country, data);
+    return this.countryRepository.save(country);
+  }
+
+  async deleteCountry(id: number): Promise<boolean> {
+    const result = await this.countryRepository.delete({ country_id: id });
+    return result.affected !== 0;
+  }
+}
+
+export default new CountryService();
