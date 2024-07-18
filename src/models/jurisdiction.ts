cb93c9e952d3eea356a64a3f9eba14305728448f
Ahmed Baig <ahmedbaig137@gmail.com>
2024-07-18 12:54:45 +0500

    added jurisdiction table in type ORM

--- src/models/jurisdiction.ts
+++ src/models/jurisdiction.ts
@@ -1,81 +1,32 @@
-// import { DataTypes, Model, Optional } from 'sequelize';
-// import sequelize from '../config/db';
-
-// interface JurisdictionAttributes {
-//   id: number;
-//   UserFeeShare: number;
-//   tax: number;
-//   discount: object;
-//   buy_margin: number;
-//   sell_margin: number;
-//   FassetFee: number;
-//   dinariConstantFee: number;
-//   dinariPercentageFee: number;
-// }
-
-// interface JurisdictionCreationAttributes extends Optional<JurisdictionAttributes, 'id'> {}
-
-// class Jurisdiction extends Model<JurisdictionAttributes, JurisdictionCreationAttributes>
-//   implements JurisdictionAttributes {
-//   public id!: number;
-//   public UserFeeShare!: number;
-//   public tax!: number;
-//   public discount!: object;
-//   public buy_margin!: number;
-//   public sell_margin!: number;
-//   public FassetFee!: number;
-//   public dinariConstantFee!: number;
-//   public dinariPercentageFee!: number;
-
-//   public readonly createdAt!: Date;
-//   public readonly updatedAt!: Date;
-// }
-
-// Jurisdiction.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     UserFeeShare: {
-//       type: DataTypes.FLOAT,
-//       allowNull: false,
-//     },
-//     tax: {
-//       type: DataTypes.FLOAT,
-//       allowNull: false,
-//     },
-//     discount: {
-//       type: DataTypes.JSON,
-//       allowNull: false,
-//     },
-//     buy_margin: {
-//       type: DataTypes.FLOAT,
-//       allowNull: false,
-//     },
-//     sell_margin: {
-//       type: DataTypes.FLOAT,
-//       allowNull: false,
-//     },
-//     FassetFee: {
-//       type: DataTypes.FLOAT,
-//       allowNull: false,
-//     },
-//     dinariConstantFee: {
-//       type: DataTypes.FLOAT,
-//       allowNull: false,
-//     },
-//     dinariPercentageFee: {
-//       type: DataTypes.FLOAT,
-//       allowNull: false,
-//     },
-//   },
-//   {
-//     sequelize,
-//     modelName: 'Jurisdiction',
-//     timestamps: true,
-//   }
-// );
-
-// export default Jurisdiction;
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn
+  } from 'typeorm';
+  @Entity()
+  export class Jurisdiction {
+    @PrimaryGeneratedColumn()
+    id!: number;
+    @Column('float')
+    UserFeeShare!: number;
+    @Column('float')
+    tax!: number;
+    @Column('json')
+    discount!: object;
+    @Column('float')
+    buy_margin!: number;
+    @Column('float')
+    sell_margin!: number;
+    @Column('float')
+    FassetFee!: number;
+    @Column('float')
+    dinariConstantFee!: number;
+    @Column('float')
+    dinariPercentageFee!: number;
+    @CreateDateColumn()
+    createdAt!: Date;
+    @UpdateDateColumn()
+    updatedAt!: Date;
+  }
