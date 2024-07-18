77bd6a6724ef8f9daf68bd128de46db5fe193253
Sameer <sameerkhan.as100@gmail.com>
2024-07-18 13:06:16 +0500

    Unstaged changes in country

--- src/models/jurisdiction.ts
+++ src/models/jurisdiction.ts
@@ -5,28 +5,40 @@ import {
     CreateDateColumn,
     UpdateDateColumn
   } from 'typeorm';
+  
   @Entity()
   export class Jurisdiction {
     @PrimaryGeneratedColumn()
     id!: number;
+  
     @Column('float')
     UserFeeShare!: number;
+  
     @Column('float')
     tax!: number;
+  
     @Column('json')
     discount!: object;
+  
     @Column('float')
     buy_margin!: number;
+  
     @Column('float')
     sell_margin!: number;
+  
     @Column('float')
     FassetFee!: number;
+  
     @Column('float')
     dinariConstantFee!: number;
+  
     @Column('float')
     dinariPercentageFee!: number;
+  
     @CreateDateColumn()
     createdAt!: Date;
+  
     @UpdateDateColumn()
     updatedAt!: Date;
   }
+  
