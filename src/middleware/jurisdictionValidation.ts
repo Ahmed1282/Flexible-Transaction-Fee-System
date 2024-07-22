277eef6d895cce935f9cd61de4adc5b2b0632363
Sameer <sameerkhan.as100@gmail.com>
2024-07-22 15:59:43 +0500

    Few changes

--- src/middleware/jurisdictionValidation.ts
+++ src/middleware/jurisdictionValidation.ts
@@ -6,12 +6,24 @@ export const validateJurisdiction = [
   check('jurisdiction_name').notEmpty().withMessage('Jurisdiction name cannot be empty'),
   check('UserFeeShare').isFloat().withMessage('User Fee Share must be a float'),
   check('tax').isFloat().withMessage('Tax must be a float'),
-  check('discount').isObject().withMessage('Discount must be a valid JSON object'),
+  check('jurisdiction_discount')
+    .optional()
+    .isObject().withMessage('Jurisdiction discount must be an object')
+    .custom((value) => {
+      if (!value.name || typeof value.name !== 'string') {
+        throw new Error('Jurisdiction discount must have a name property of type string');
+      }
+      if (!value.value || typeof value.value !== 'number') {
+        throw new Error('Jurisdiction discount must have a value property of type number');
+      }
+      return true;
+    }),
   check('buy_margin').isFloat().withMessage('Buy margin must be a float'),
   check('sell_margin').isFloat().withMessage('Sell margin must be a float'),
   check('FassetFee').isFloat().withMessage('Fasset Fee must be a float'),
   check('dinariConstantFee').isFloat().withMessage('Dinari Constant Fee must be a float'),
   check('dinariPercentageFee').isFloat().withMessage('Dinari Percentage Fee must be a float'),
+  check('juris_discount_applied').isFloat().withMessage('Jurisdiction discount applied must be a float'),
   
   (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
